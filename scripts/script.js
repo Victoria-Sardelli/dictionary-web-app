f9aee416589158a6002316ea09474438eb1494ac
Victoria Sardelli <vlsardelli@outlook.com>
2023-03-30 17:18:59 +0900

    Moves font dropdown code into new js file for organization

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,5 @@
 "use strict";
 
-// dropdown elements
-const fontDropdown = document.querySelector(".dropdown");
-const fontDropdownBtn = document.querySelector(".dropdown-toggle-btn");
-const fontDropdownBtnText = document.querySelector(
-    ".dropdown-toggle-btn__text"
-);
-const fontDropdownContent = document.querySelector(".dropdown-content");
-const fontDropdownOptions = document.querySelectorAll(".dropdown-option-btn");
-
-/* Show/hide font dropdown when button is clicked */
-fontDropdownBtn.addEventListener("click", () => {
-    fontDropdownContent.classList.toggle("show");
-});
-
-/* Update font when a font option is clicked from dropdown */
-const updateFont = function (e) {
-    fontDropdownBtnText.textContent = e.target.dataset.font;
-};
-
-for (const dropdownOption of fontDropdownOptions) {
-    dropdownOption.addEventListener("click", updateFont);
-}
-
-/* Hide font dropdown content if user clicks outside of dropdown */
-const hideDropdownIfClickedOutside = function (e) {
-    let currentTarget = e.target;
-    while (currentTarget && currentTarget !== fontDropdown) {
-        currentTarget = currentTarget.parentNode;
-    }
-    if (currentTarget !== fontDropdown)
-        fontDropdownContent.classList.remove("show");
-};
-document.addEventListener("click", hideDropdownIfClickedOutside);
-
 /* Form Submission*/
 const form = document.querySelector(".form");
 const searchBar = document.querySelector(".search-bar");
