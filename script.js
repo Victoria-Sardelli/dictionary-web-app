350727751a6d84e7313f2462aacdde01dab13442
Victoria Sardelli <vlsardelli@outlook.com>
2023-03-30 17:15:37 +0900

    Adds search bar and input validation

--- script.js
+++ script.js
@@ -33,3 +33,29 @@ const hideDropdownIfClickedOutside = function (e) {
         fontDropdownContent.classList.remove("show");
 };
 document.addEventListener("click", hideDropdownIfClickedOutside);
+
+/* Form Submission*/
+const form = document.querySelector(".form");
+const searchBar = document.querySelector(".search-bar");
+const searchError = document.querySelector(".error-msg");
+
+searchBar.addEventListener("input", (e) => {
+    if (searchBar.validity.valid) {
+        searchError.textContent = "";
+        searchError.classList.remove("active");
+        searchBar.classList.remove("error");
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    if (!searchBar.validity.valid) {
+        // show error message if user submitted empty search
+        searchError.textContent = "Whoops, can't be empty...";
+        searchError.classList.add("active");
+        searchBar.classList.add("error");
+    } else {
+        // get data using user input
+        console.log(`get data: ${searchBar.value}`);
+    }
+    e.preventDefault();
+});
