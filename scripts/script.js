253b9bd86853d17e74cf1aea8f7bbb94cc138c39
Victoria Sardelli <vlsardelli@outlook.com>
2023-03-31 00:26:24 +0900

    Adds logic to handle 404 error responses and display results with appropriate styles on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -5,26 +5,45 @@ const form = document.querySelector(".form");
 const searchBar = document.querySelector(".search-bar");
 const searchError = document.querySelector(".error-msg");
 
-searchBar.addEventListener("input", (e) => {
-    if (searchBar.validity.valid) {
-        searchError.textContent = "";
-        searchError.classList.remove("active");
-        searchBar.classList.remove("error");
+const searchResultsSection = document.querySelector(".search-results");
+
+/* Removes all child elements from DOM node */
+const removeChildren = function (element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
     }
-});
+};
 
 /* Displays 404 message when word could not be found by dictionary api */
 const handle404 = function (data) {
-    console.log("Aw, there was a 404 error...");
-    console.log(data);
+    const emoji = document.createElement("p");
+    emoji.className = "emoji";
+    emoji.textContent = "😕";
+
+    const title = document.createElement("h1");
+    title.className = "search-error-title";
+    title.textContent = data.title ?? "No definitions found";
+
+    const subtitle = document.createElement("p");
+    subtitle.className = "search-error-subtitle";
+    subtitle.textContent = `${
+        data.message ?? "Sorry, we couldn't find any definitions."
+    } ${data.resolution ?? "Please try the search again later."}`;
+
+    removeChildren(searchResultsSection);
+    searchResultsSection.append(emoji, title, subtitle);
 };
 
 /* Displays word-related information receieved from dictionary api */
 const displayResults = function (data) {
-    console.log("Yay! You got data.");
-    console.log(data);
+    const word = document.createElement("h1");
+    word.textContent = "word";
+
+    removeChildren(searchResultsSection);
+    searchResultsSection.appendChild(word);
 };
 
+/* Gets definitions from dictionary api and displays results on screen */
 form.addEventListener("submit", (e) => {
     if (!searchBar.validity.valid) {
         // show error message if user submitted empty search
@@ -45,4 +64,13 @@ form.addEventListener("submit", (e) => {
             .catch((error) => console.error(error));
     }
     e.preventDefault();
+});
+
+/* Removes warning styles upon user input when search bar has valid content */
+searchBar.addEventListener("input", (e) => {
+    if (searchBar.validity.valid) {
+        searchError.textContent = "";
+        searchError.classList.remove("active");
+        searchBar.classList.remove("error");
+    }
 });
